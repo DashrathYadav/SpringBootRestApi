4739be1d9d6ef3eb01e852db7809be192f948e12
Dashrath <sandip9028162859@gmail.com>
2023-09-23 12:53:29 +0530

    down graded to tomcat 9

--- src/main/java/com/example/mongoDBRestApi/MongoDbRestApiApplication.java
+++ src/main/java/com/example/mongoDBRestApi/MongoDbRestApiApplication.java
@@ -5,11 +5,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class MongoDbRestApiApplication implements CommandLineRunner {
 	
 	public static final Logger logger =LoggerFactory.getLogger(MongoDbRestApiApplication.class);
+	
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
