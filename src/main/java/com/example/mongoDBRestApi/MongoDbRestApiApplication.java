5570b2d6e31827f6760f8655fc04849960c50808
Dashrath <sandip9028162859@gmail.com>
2023-09-23 09:35:30 +0530

    logger added

--- src/main/java/com/example/mongoDBRestApi/MongoDbRestApiApplication.java
+++ src/main/java/com/example/mongoDBRestApi/MongoDbRestApiApplication.java
@@ -1,14 +1,28 @@
 package com.example.mongoDBRestApi;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MongoDbRestApiApplication {
+public class MongoDbRestApiApplication implements CommandLineRunner {
+	
+	public static final Logger logger =LoggerFactory.getLogger(MongoDbRestApiApplication.class);
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		
+		logger.info("Hello Dashrath it is main class")
 		SpringApplication.run(MongoDbRestApiApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		logger.info("Its my second logger dashrath");
+		
 	}
 
 }
--- src/test/java/com/example/mongoDBRestApi/MongoDbRestApiApplicationTests.java
+++ src/test/java/com/example/mongoDBRestApi/MongoDbRestApiApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example.mongoDBRestApi;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class MongoDbRestApiApplicationTests {
 
+	public static final Logger logger =LoggerFactory.getLogger(MongoDbRestApiApplication.class);
+	
 	@Test
 	void contextLoads() {
+		
+		logger.info("This is test class runnign Dashrath");
+		assertEquals(true,true);
 	}
 
 }
